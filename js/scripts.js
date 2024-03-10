2707417bc73654ec491b81ac904d4f39a46280cb
shesjessy <benitezjessicae@gmail.com>
2024-03-10 00:11:10 -0800

    Task 1.3 - Added a "for" loop with a conditional stating if height > 1 then print "Wow, that's big!"

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,19 @@ let pokemonList = [
   {name: "Bulbasaur", height: 0.7, types: ['grass', 'poison']},
   {name: "Charizard", height: 1.7, types: ['fire', 'flying']},
   {name: "Jigglypuff", height: 0.5, types: ['fairy', 'normal']}
-];  
+];
+
+//Use document.write() 
+//for example, “Bulbasaur (height: 7)”
+
+//check if the height is above a certain value
+//if yes= "Wow, that's big!"
+//set up the conditional so that only one Pokémon has the label “Wow, that’s big!”
+//example: “Bulbasaur (height: 7) - Wow, that’s big!”
+
+for (let i=0; i<pokemonList.length; i++){
+  document.write (pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+  if (pokemonList[i].height > 1.0){
+  document.write (" - Wow, that's a big one!");
+  }
+}
