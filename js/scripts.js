f88c0a8c6033d85b58d40d2cbd265156345c3d69
shesjessy <benitezjessicae@gmail.com>
2024-05-08 10:49:11 -0700

    I used a forEach() function intead of the for loop over the Pokemon in my pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,8 @@ let pokemonList = [
 //set up the conditional so that only one Pokémon has the label “Wow, that’s big!”
 //example: “Bulbasaur (height: 7) - Wow, that’s big!”
 
-for (let i=0; i<pokemonList.length; i++){
-  document.write (pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
-  if (pokemonList[i].height > 1.0){
-  document.write (" - Wow, that's a big one!");
-  }
-}
+pokemonList.forEach(function(pokemon) {
+document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
+if (pokemon.height > 1.0){
+document.write (" - Wow, that's a big one!");
+}});
