d9d641ace84530af6419849f0ec81466b6931dac
shesjessy <benitezjessicae@gmail.com>
2024-05-09 22:08:10 -0700

    modified code for exercise 1.5 task

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,50 @@
+let pokemonRepository = (function (){
+
 let pokemonList = [
   {name: "Bulbasaur", height: 0.7, types: ['grass', 'poison']},
   {name: "Charizard", height: 1.7, types: ['fire', 'flying']},
   {name: "Jigglypuff", height: 0.5, types: ['fairy', 'normal']}
 ];
 
-let pokemonRepository = (function (){
-  let pokemonList = [];
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-    // adds one pokemon to the pokemonList
+return {
+  getAll: function () {
+    return pokemonList;
+  },
+  add: function (item) {
+    pokemonList.push (item);
   }
-
-function getAll () {
-  return pokemonList;
-  // returns all pokemon in the pokemonList
 }
 
-return{
+function add(item) {
+  pokemonList.push(item);
+}
+
+function getAll() {
+  return pokemonList;
+}
+
+return {
+  getAll: getAll,
   add: add,
-  getAll: getAll
-};
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-  console.log(pokemon.name);
-})});
-
-(function() {
-  pokemonList.forEach(function(pokemon) {
-      document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
-      if (pokemon.height > 1.0){
-          document.write(" - Wow, that's a big one!");
-      }
-  });
+}
 })();
+
+pokemonList.forEach(function (pokemon) {
+  document.write(
+    pokemon.name + " (height: " + pokemon.height + ")"
+  );
+  if (pokemon.height > 1.0) {
+    document.write(" - Wow that's a big one!");
+  }
+  document.write("<br>");
+});
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  document.write(
+    pokemon.name + " (height: " + pokemon.height + ")"
+  );
+  if (pokemon.height > 1.0) {
+    document.write(" - Wow that's a big one!");
+  }
+  document.write("<br>");
+});
